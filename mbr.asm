06bce09529bd197dc0460490f850ddffb4a8969c
kmkorym <kmkorym@gmail.com>
2021-01-28 22:35:25 +0800

    read first and second sector from floppy disk

--- mbr.asm
+++ mbr.asm
@@ -1,13 +1,34 @@
 ;code section
 bits 16
 org 0x7c00
-
-mov ebx,HELLO_STRING
+;; just print something
+mov bx,HELLO_STRING
 call printl
 
-mov ebx,HELLO_STRING
+mov bx,HELLO_STRING
 call printl
 
+;init seg registers
+mov bp, 0x8000 ; set the stack safely away from us
+mov sp, bp
+
+mov bp, 0
+mov es, bp
+
+mov bp, 0
+mov ds, bp
+
+mov bp, 0
+mov ss, bp
+
+
+
+;load 512 byte to memery and print
+mov bx, 0x9000 ; es:bx = 0x0000:0x9000 = 0x09000
+mov dh, 2 ; read 2 sectors
+call disk_load
+mov bx, 0x9000+512; first word from second loaded sector, 0xface
+call printl
 jmp $
 
 
@@ -22,11 +43,11 @@ printl:
 print:
     mov ah, 0x0e ; tty mode
     .loop_start:
-    mov al,[ebx]
+    mov al,[bx]
     int 0x10
     cmp  al,0
     je .loop_end
-    add ebx,1
+    add bx,1
     jmp .loop_start
     .loop_end:
     ;print new line
@@ -42,6 +63,7 @@ print_nl:
     int 0x10
     ret
 
+%include "disk.asm"
 
 
 ;data section
@@ -50,4 +72,11 @@ db "hello world",0
 times 510-($-$$) db 0
 dw 0xAA55
 
+MDFK_STRING:
+db "MDFK",0
+times 507 db 0
+
+MDFK_STRING2:
+db "MDFK2",0
+times 506 db 0
 
